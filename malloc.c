7c4584e1f847d58f79ba9ae3b2b7f43cf9eb80e6
ble27 <bryantle2706@outlook.com>
2026-09-19 17:30:56 -0500

    Basic malloc and allocator

---
+++ malloc.c
@@ -0,0 +1,81 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdalign.h>
+#include <pthread.h>
+
+/* 
+/ Every memory block contains a header and a memory block  
+/ The header tracks the size of the entire block and its allocation status 
+/ and contains a pointer to the next block in memory
+/ Header file is 16 bytes aligned, and it contains both the size of the memory,
+/ and a pointer to the next 16 bytes aligned header
+*/
+
+alignas(16) struct header {
+    size_t sz;
+    unsigned is_free;
+    struct header* next;
+};
+typedef struct header header_t;
+
+header_t *head, *tail;
+
+header_t* get_free_block(size_t size) {
+    header_t* curr = head;
+    /*
+    / Check if the current block is free and that the block size is 
+    / greater than requested memory size
+    */
+    while (curr) {
+        if (curr->is_free && curr->sz >= size) {
+            return curr;
+        }
+        curr = curr->next; 
+    }
+}
+/*
+/ brk pointer points to the end of the heap
+/ sbrk is used to request extra memory on the heap
+/ sbrk(+x) requests the system to allocate more memory on the heap,
+/ and sbrk(-x) is used to decrement memory on the heap
+/ use mmap for modern version
+*/ 
+pthread_mutex_t global_malloc_lock;
+void* malloc(size_t sz) {
+    size_t total_size;
+    void* block;
+    header_t *header;
+
+    if (!sz) {
+        return NULL;
+    }
+    pthread_mutex_lock(&global_malloc_lock); // acquire lock
+    header = get_free_block(sz); // start of a free block
+    if (header) {
+        header->is_free = 0;
+        pthread_mutex_unlock(&global_malloc_lock);
+        return (void*)(header + 1); // +1 = 1 * sizeof(*header) -> moves past header struct
+    }
+
+    size_t aligned_size = (sz + 15) & ~15; // rounds sz up the nearest multiple of 16
+    total_size = sizeof(header_t) + aligned_size;
+    block = sbrk(total_size); // alloc and moves pointer on the heap upward
+    if (block == (void*)-1) {
+        pthread_mutex_unlock(&global_malloc_lock);
+        return NULL;
+    }
+    header = block; // move header to where block is
+    header->sz = aligned_size;
+    header->is_free = 0;
+    header->next = NULL;
+    if (!head) {
+        head = header;
+    }
+    if (tail) {
+        tail->next = header; // before, tail represents the current last node of list
+    }
+    tail = header;
+    pthread_mutex_unlock(&global_malloc_lock);
+    return (void*)(header + 1);
+}
